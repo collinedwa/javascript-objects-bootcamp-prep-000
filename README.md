8f8cd7ffba3362aea48f2527862ded83e33922f5
Collin Edwards <collinedwards1579@gmail.com>
2018-01-23 20:10:33 +0000

    automatically backed up by learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JavaScript Objects
+keytop t# JavaScript Objects
 
 ## Objectives
 + Explain what an object is in JavaScript is
--- objects.js
+++ objects.js
@@ -1,13 +1,25 @@
+<<<<<<< HEAD
 var playlist = {artistName: "songTitle"}
 
 function updatePlaylist(){
+=======
+var playlist = {artistName: songTitle}
+
+function updatePlaylist(artistName, songTitle){
+>>>>>>> 5b3b8187ca66923877af65df126428c1a6ee6f91
   delete playlist.artistName
   return Object.assign({}, playlist, { 'Phil Ochs': "Here's to the State of Mississippi"})
 }
 
+<<<<<<< HEAD
 function removeFromPlaylist(){
   delete playlist.artistName
   Object.assign({}, playlist, { Kanye: "Gold Digger" })
   delete playlist.Kanye
   return playlist
+=======
+function removeFromPlaylist(playlist, artistName){
+  playlist = {Kanye: "Gold Digger"}
+  return delete playlist.Kanye
+>>>>>>> 5b3b8187ca66923877af65df126428c1a6ee6f91
 }
